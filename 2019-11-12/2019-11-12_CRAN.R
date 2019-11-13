0d83831a989e2a23bca1813ae19dfddea17b96e2
Yuliya Astapova <yva2@illinois.edu>
2019-11-13 11:31:51 -0500

    cosmetic improvements to chart

--- 2019-11-12/2019-11-12_CRAN.R
+++ 2019-11-12/2019-11-12_CRAN.R
@@ -26,7 +26,9 @@ melted$language = factor(melted$language, levels=unique(melted$language))
 chart = melted %>%
   ggplot(aes(x = language, y = value, fill=variable)) +
   geom_col(position="dodge") +
-  scale_y_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0), 
+                     breaks = seq(0, 3000, 500),
+                     minor_breaks = seq(250, 3100, 250)) +
   xlab(NULL) +
   ylab("Lines per Package") +
   labs(title = "Lines of Code, Comments, and Blanks per Language in CRAN Packages") +
@@ -35,9 +37,14 @@ chart = melted %>%
         axis.line.x.bottom = element_line(color = "black", size = 0.1),
         axis.line.y.left = element_line(color = "black", size = 0.1),
         panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(size = 0.75),
+        panel.grid.minor.y = element_line(size = 0.75),
         panel.background = element_rect(fill = NA),
         panel.ontop = TRUE,
         plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5))
+        plot.subtitle = element_text(hjust = 0.5),
+        legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.spacing.x = unit(0.25, "cm"))
 
 print(chart)
